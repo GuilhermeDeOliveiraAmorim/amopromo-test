37171841d8c70c712d2dc30ba66e870424afabe6
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-11-19 15:47:44 -0300

    feat: hero section

---
+++ components/header/hero/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import HeroStyles from './Hero.module.scss';
+
+function Hero() {
+    return (
+        <div className={HeroStyles.Hero}>
+            <h1>
+                Tranquilidade para viver mais
+            </h1>
+            <div>
+                <h2>
+                    Viva mais e melhor com os benefícios que um serguro de vida te oferece
+                </h2>
+                <button>
+                    Faça sua cotação
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export { Hero };
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import IHeader from "../../interfaces/IHeader";
 import { Contact } from "./contact";
 import { Nav } from "./nav";
+import { Hero } from "./hero";
 import HeaderStyles from './Header.module.scss';
 
 function Header(props: IHeader) {
     const { contact, nav } = props;
-
     return (
         <div className={HeaderStyles.Header}>
             <Contact {...contact} />
             <Nav {...nav} />
+            <Hero />
         </div>
     )
 }
